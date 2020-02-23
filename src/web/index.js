42e73d14a176ab11e1d49ed93f789948fc92c3aa
prathamesh palyekar <pratham2604@gmail.com>
2020-02-23 19:47:57 +0530

    Add Semantic UI and sass

--- src/web/index.js
+++ src/web/index.js
@@ -6,6 +6,8 @@ import { PersistGate } from 'redux-persist/lib/integration/react';
 import configureStore from './store/index';
 import * as serviceWorker from './serviceWorker/index';
 import Routes from './routes/index';
+import 'semantic-ui-css/semantic.min.css'
+import '../styles/styles.scss';
 
 const { persistor, store } = configureStore();
 
